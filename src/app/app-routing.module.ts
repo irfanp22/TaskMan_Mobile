5a4ffab8080d7363e75cad9d0b913d62e30eec67
irfanp22 <irfanpriatna22@gmail.com>
2023-10-06 11:02:17 +0700

    task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'task',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    loadChildren: () => import('./folder/folder.module').then(m => m.FolderPageModule)
   },
   {
     path: 'task',
-    loadChildren: () => import('./task/task.module').then( m => m.TaskPageModule)
+    loadChildren: () => import('./task/task.module').then(m => m.TaskPageModule)
   }
 ];
 
@@ -23,4 +23,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/task/task.page.ts
+++ src/app/task/task.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-task',
@@ -6,10 +8,131 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task.page.scss'],
 })
 export class TaskPage implements OnInit {
+  task: any = [];
+  modal_tambah = false;
+  modal_edit = false;
+  id: any;
+  title: any;
+  desc: any;
+  status: any;
 
-  constructor() { }
+  constructor(public _apiService: ApiService, private modal: ModalController) { }
 
   ngOnInit() {
+    this.getTask();
+  }
+  getTask() {
+    this._apiService.tampil('tampil.php').subscribe({
+      next: (res: any) => {
+        console.log('sukses', res);
+        this.task = res;
+      },
+      error: (err: any) => {
+        console.log(err);
+      },
+    })
+  }
+  reset_model() {
+    this.id = null;
+    this.title = '';
+    this.desc = '';
+    this.status = '';
+  }
+  open_modal_tambah(isOpen: boolean) {
+    this.modal_tambah = isOpen;
+    this.reset_model();
+    this.modal_tambah = true;
+    this.modal_edit = false;
+  }
+  open_modal_edit(isOpen: boolean, idget: any) {
+    this.modal_edit = isOpen;
+    this.id = idget;
+    console.log(this.id);
+    this.getTaskId(this.id);
+    this.modal_tambah = false;
+    this.modal_edit = true;
+  }
+  cancel() {
+    this.modal.dismiss();
+    this.modal_tambah = false;
+    this.reset_model();
+  }
+  addTask() {
+    if (this.title != '' && this.desc != '') {
+      let data = {
+        title: this.title,
+        desc: this.desc,
+        status: 0
+      };
+
+      this._apiService.tambah(data, '/tambah.php').subscribe({
+        next: (hasil: any) => {
+          this.reset_model();
+          console.log('berhasil tambah task');
+          this.getTask();
+          this.modal_tambah = false;
+          this.modal.dismiss();
+        },
+        error: (err: any) => {
+          console.error('gagal tambah task', err);
+        },
+      });
+    } else {
+      console.log('gagal tambah task karena masih ada data yg kosong');
+    }
+  }
+
+
+
+  delTask(id: any) {
+    this._apiService.hapus(id, '/hapus.php?id=').subscribe({
+      next: (res: any) => {
+        console.log('sukses', res);
+        this.getTask();
+        console.log('berhasil hapus data');
+      },
+      error: (error: any) => {
+        console.log('gagal');
+      }
+    })
+  }
+  getTaskId(id: any) {
+    this._apiService.lihat(id,
+      '/lihat.php?id=').subscribe({
+        next: (hasil: any) => {
+          console.log('sukses', hasil);
+          let task = hasil;
+          this.id = task.id;
+          this.title = task.title;
+          this.desc = task.desc;
+          this.status = task.status
+        },
+        error: (error: any) => {
+          console.log('gagal ambil data');
+        }
+      })
+  }
+  editTask() {
+    let data = {
+      id: this.id,
+      title: this.title,
+      desc: this.desc,
+      status: this.status,
+    };
+
+    this._apiService.edit(data, '/edit.php').subscribe({
+      next: (hasil: any) => {
+        console.log(hasil);
+        this.reset_model();
+        this.getTask();
+        console.log('berhasil edit task');
+        this.modal_edit = false;
+        this.modal.dismiss();
+      },
+      error: (err: any) => {
+        console.error('gagal edit task', err);
+      },
+    });
   }
 
 }
